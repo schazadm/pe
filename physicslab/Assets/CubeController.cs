245db82d09048cea73d04f7b797fe460673547f6
Schazad Mohammad <mohamsch@students.zhaw.ch>
2023-03-06 13:15:22 +0100

    init with the lab 💥🐱‍🏍

---
+++ physicslab/Assets/CubeController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.IO;
+
+/*
+    Accelerates the cube to which it is attached, modelling an harmonic oscillator.
+    Writes the position, velocity and acceleration of the cube to a CSV file.
+    
+    Remark: For use in "Physics Engines" module at ZHAW, part of physics lab
+    Author: kemf
+    Version: 1.0
+*/
+public class CubeController : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    public int springConstant; // N/m
+
+    private float currentTimeStep; // s
+    
+    private List<List<float>> timeSeries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        timeSeries = new List<List<float>>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // FixedUpdate can be called multiple times per frame
+    void FixedUpdate() {
+        float forceX; // N
+
+        // Calculate spring force on body for x component of force vector
+        forceX = -rigidBody.position.x * springConstant;
+        rigidBody.AddForce(new Vector3(forceX, 0f, 0f));
+
+        currentTimeStep += Time.deltaTime;
+        timeSeries.Add(new List<float>() {currentTimeStep, rigidBody.position.x, rigidBody.velocity.x, forceX});
+    }
+
+    void OnApplicationQuit() {
+        WriteTimeSeriesToCSV();
+    }
+
+    void WriteTimeSeriesToCSV() {
+        using (var streamWriter = new StreamWriter("time_series.csv")) {
+            streamWriter.WriteLine("t,x(t),v(t),F(t) (added)");
+            
+            foreach (List<float> timeStep in timeSeries) {
+                streamWriter.WriteLine(string.Join(",", timeStep));
+                streamWriter.Flush();
+            }
+        }
+    }
+}
---
+++ physicslab/README.md
@@ -0,0 +1,11 @@
+# Physics Lab
+A Unity project modelling an harmonic oscillator. For use in “Physics Engines” module at ZHAW. This Unity project requires the Unity Editor LTS release [2021.3.17f1](https://unity.com/releases/editor/qa/lts-releases?version=2021.3 "Unity's LTS releases web page").
+
+![Physics Lab scene](physics_lab_scene.png "Physics Lab scene")
+
+## How to run 
+1. Download (or clone) this repository.
+2. In the Unity Hub, add the repository as a project and open it.
+3. In the Unity Editor, open the *HarmonicOscillator* scene in the *Scenes* directory.
+4. Click on the blue cube and adjust its mass and the spring constant in the inspector window.
+4. Press the *Play* button.
