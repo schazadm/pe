7e51e4a5403953ed94398203c864bf9d9d2aba31
Schazad Mohammad <schazad.mohammad@hotmail.com>
2023-04-23 16:35:00 +0200

    continued part02

--- physicslab/Assets/Scripts/part02/StatsLoggerController.cs
+++ physicslab/Assets/Scripts/part02/StatsLoggerController.cs
@@ -4,8 +4,8 @@ using System.IO;
 
 public class StatsLoggerController : MonoBehaviour
 {
-    public Rigidbody cubeOne;
-    public Rigidbody cubeTwo;
+    public Rigidbody c_1;
+    public Rigidbody c_2;
     private float currentTimeStep; // s
     private float currentVelocityX; // m/s
     private float currentImpulseX; // N*s
@@ -20,25 +20,16 @@ public class StatsLoggerController : MonoBehaviour
     void FixedUpdate()
     {
         currentTimeStep += Time.deltaTime;
-        // currentVelocityX = rb.velocity.x;
-        // currentImpulseX = rb.mass * currentVelocityX;
-        // currentForce = (rb.velocity.x / currentTimeStep) * rb.mass;
-
-        // timeSeries.Add(new List<float>() {
-        //     currentTimeStep,
-        //     rb.position.x,
-        //     rb.velocity.x,
-        //     currentImpulseX,
-        //     currentForce,
-        // });
-
         timeSeries.Add(new List<float>() {
             currentTimeStep,
-            cubeOne.position.x ,
-            cubeTwo.position.x,
-            cubeOne.velocity.x, cubeTwo.velocity.x,
-            cubeOne.velocity.x * cubeOne.mass,
-            cubeTwo.velocity.x * cubeTwo.mass
+            c_1.position.x,
+            c_2.position.x,
+            c_1.velocity.x,
+            c_2.velocity.x,
+            c_1.velocity.x * c_1.mass,
+            c_2.velocity.x * c_2.mass,
+            (c_1.velocity.x / currentTimeStep) * c_1.mass, // F=a*m=(v/deltaT)*m
+            (c_2.velocity.x / currentTimeStep) * c_2.mass,
         });
     }
 
@@ -49,9 +40,9 @@ public class StatsLoggerController : MonoBehaviour
 
     void WriteTimeSeriesToCSV()
     {
-        using (var streamWriter = new StreamWriter("time_series_cube_1.csv"))
+        using (var streamWriter = new StreamWriter("time_series.csv"))
         {
-            streamWriter.WriteLine("t,x(t),v(t),p(t),F(t)");
+            streamWriter.WriteLine("t,x1(t),x2(t),v1(t),v2(t),p1(t),p2(t),F1(t),F2(t)");
             foreach (List<float> timeStep in timeSeries)
             {
                 streamWriter.WriteLine(string.Join(",", timeStep));
--- py_stats/Graphische_Darstellungen.py
+++ py_stats/Graphische_Darstellungen.py
@@ -7,96 +7,98 @@ Created on Thu Mar  2 21:13:46 2023
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-df = pd.read_csv('time_series_exercise2.csv')
-# df = pd.read_csv('time_series_teilaufgabe_02.csv')
+df = pd.read_csv('time_series.csv')
 
 print(df.head())
 print(df.columns)
 
-plt.figure(1)
-# plt.plot(df['t'], df['x_cubeOne(t)'], color='green') # Teilaufgabe_01
-plt.plot(df['t'], df['x_cubeOne(t)'], color='green')  # Teilaufgabe_02
-plt.xlabel('Zeit t [s]')
-plt.ylabel('Ortsvektor r [m]')
-plt.title('Ort in Abhängigkeit der Zeit')
-# plt.title('Teilaufgabe_02: Grafische Darstellung des Ortes Zeitreihe_02')
-plt.legend(['Würfel 1'])
-plt.grid()
-
-plt.figure(2)
-# plt.plot(df['t'], df['x_cubeOne(t)'], color='green') # Teilaufgabe_01
-plt.plot(df['t'], df['x_cubeTwo(t)'], color='green')  # Teilaufgabe_02
-plt.xlabel('Zeit t [s]')
-plt.ylabel('Ortsvektor r [m]')
-plt.title('Ort in Abhängigkeit der Zeit')
-# plt.title('Teilaufgabe_02: Grafische Darstellung des Ortes Zeitreihe_02')
-plt.legend(['Würfel 2'])
-plt.grid()
-
-plt.figure(3)
-plt.plot(df['t'], df['v_cubeOne(t)'], color='purple')
-plt.xlabel('Zeit t [s]')
-plt.ylabel('Geschwindigkeit v [m/s]')
-plt.title('Geschwindigkeit in Abhängigkeit der Zeit')
-# plt.title('Teilaufgabe_02: Grafische Darstellung des Geschwindigkeit Zeitreihe_02')
-plt.legend(['Würfel 1'])
-plt.grid()
-
-plt.figure(4)
-plt.plot(df['t'], df['v_cubeTwo(t)'], color='purple')
-plt.xlabel('Zeit t [s]')
-plt.ylabel('Geschwindigkeit v [m/s]')
-plt.title('Geschwindigkeit in Abhängigkeit der Zeit')
-# plt.title('Teilaufgabe_02: Grafische Darstellung des Geschwindigkeit Zeitreihe_02')
-plt.legend(['Würfel 2'])
-plt.grid()
-
-plt.figure(5)
-plt.plot(df['t'], df['p_cubeOne(t)'], color='red')
-plt.xlabel('Zeit t [s]')
-plt.ylabel('Impuls p [Ns]')
-plt.title('Impuls in Abhängigkeit der Zeit')
-# plt.title('Teilaufgabe_02: Grafische Darstellung des Kraft Zeitreihe_02')
-plt.legend(['Würfel 1'])
-plt.grid()
-
-plt.figure(6)
-plt.plot(df['t'], df['p_cubeTwo(t)'], color='red')
-plt.xlabel('Zeit t [s]')
-plt.ylabel('Impuls p [Ns]')
-plt.title('Impuls in Abhängigkeit der Zeit')
-# plt.title('Teilaufgabe_02: Grafische Darstellung des Kraft Zeitreihe_02')
-plt.legend(['Würfel 2'])
-plt.grid()
+# plt.figure(1)
+# # plt.plot(df['t'], df['x1(t)'], color='green') # Teilaufgabe_01
+# plt.plot(df['t'], df['x1(t)'], color='green')  # Teilaufgabe_02
+# plt.xlabel('Zeit t [s]')
+# plt.ylabel('Ortsvektor r [m]')
+# plt.title('Ort in Abhängigkeit der Zeit')
+# # plt.title('Teilaufgabe_02: Grafische Darstellung des Ortes Zeitreihe_02')
+# plt.legend(['Würfel 1'])
+# plt.grid()
+#
+# plt.figure(2)
+# # plt.plot(df['t'], df['x1(t)'], color='green') # Teilaufgabe_01
+# plt.plot(df['t'], df['x2(t)'], color='green')  # Teilaufgabe_02
+# plt.xlabel('Zeit t [s]')
+# plt.ylabel('Ortsvektor r [m]')
+# plt.title('Ort in Abhängigkeit der Zeit')
+# # plt.title('Teilaufgabe_02: Grafische Darstellung des Ortes Zeitreihe_02')
+# plt.legend(['Würfel 2'])
+# plt.grid()
+#
+# plt.figure(3)
+# plt.plot(df['t'], df['v1(t)'], color='purple')
+# plt.xlabel('Zeit t [s]')
+# plt.ylabel('Geschwindigkeit v [m/s]')
+# plt.title('Geschwindigkeit in Abhängigkeit der Zeit')
+# # plt.title('Teilaufgabe_02: Grafische Darstellung des Geschwindigkeit Zeitreihe_02')
+# plt.legend(['Würfel 1'])
+# plt.grid()
+#
+# plt.figure(4)
+# plt.plot(df['t'], df['v2(t)'], color='purple')
+# plt.xlabel('Zeit t [s]')
+# plt.ylabel('Geschwindigkeit v [m/s]')
+# plt.title('Geschwindigkeit in Abhängigkeit der Zeit')
+# # plt.title('Teilaufgabe_02: Grafische Darstellung des Geschwindigkeit Zeitreihe_02')
+# plt.legend(['Würfel 2'])
+# plt.grid()
+#
+# plt.figure(5)
+# plt.plot(df['t'], df['p1(t)'], color='red')
+# plt.xlabel('Zeit t [s]')
+# plt.ylabel('Impuls p [Ns]')
+# plt.title('Impuls in Abhängigkeit der Zeit')
+# # plt.title('Teilaufgabe_02: Grafische Darstellung des Kraft Zeitreihe_02')
+# plt.legend(['Würfel 1'])
+# plt.grid()
+#
+# plt.figure(6)
+# plt.plot(df['t'], df['p2(t)'], color='red')
+# plt.xlabel('Zeit t [s]')
+# plt.ylabel('Impuls p [Ns]')
+# plt.title('Impuls in Abhängigkeit der Zeit')
+# # plt.title('Teilaufgabe_02: Grafische Darstellung des Kraft Zeitreihe_02')
+# plt.legend(['Würfel 2'])
+# plt.grid()
 
 plt.figure(7)
-# plt.plot(df['t'], df['x_cubeOne(t)'], color='green') # Teilaufgabe_01
-plt.plot(df['t'], df['x_cubeOne(t)'], color='red')  # Teilaufgabe_02
-plt.plot(df['t'], df['x_cubeTwo(t)'], color='green')  # Teilaufgabe_02
+# plt.plot(df['t'], df['x1(t)'], color='green') # Teilaufgabe_01
+plt.plot(df['t'], df['x1(t)'], color='blue')  # Teilaufgabe_02
+plt.plot(df['t'], df['x2(t)'], color='red')  # Teilaufgabe_02
 plt.xlabel('Zeit t [s]')
 plt.ylabel('Ortsvektor r [m]')
-plt.title('Ort in Abhängigkeit der Zeit')
+plt.title('Ort in Abhängigkeit der Zeit\nx(t)')
 # plt.title('Teilaufgabe_02: Grafische Darstellung des Ortes Zeitreihe_02')
 plt.legend(['Würfel 1', 'Würfel 2'])
 plt.grid()
 
 plt.figure(8)
-plt.plot(df['t'], df['v_cubeOne(t)'], color='red')
-plt.plot(df['t'], df['v_cubeTwo(t)'], color='green')
+plt.plot(df['t'], df['v1(t)'], color='blue')
+plt.plot(df['t'], df['v2(t)'], color='red')
 plt.xlabel('Zeit t [s]')
 plt.ylabel('Geschwindigkeit v [m/s]')
-plt.title('Geschwindigkeit in Abhängigkeit der Zeit')
+plt.title('Geschwindigkeit in Abhängigkeit der Zeit\nv(t)')
 # plt.title('Teilaufgabe_02: Grafische Darstellung des Geschwindigkeit Zeitreihe_02')
 plt.legend(['Würfel 1', 'Würfel 2'])
 plt.grid()
 
 plt.figure(9)
-plt.plot(df['t'], df['p_cubeOne(t)'], color='red')
-plt.plot(df['t'], df['p_cubeTwo(t)'], color='green')
+plt.plot(df['t'], df['p1(t)'], color='blue')
+plt.plot(df['t'], df['p2(t)'], color='red')
 plt.xlabel('Zeit t [s]')
 plt.ylabel('Impuls p [Ns]')
-plt.title('Impuls in Abhängigkeit der Zeit')
+plt.title('Impuls in Abhängigkeit der Zeit\np(t)')
 # plt.title('Teilaufgabe_02: Grafische Darstellung des Kraft Zeitreihe_02')
 plt.legend(['Würfel 1', 'Würfel 2'])
 plt.grid()
+
+plt.show()
